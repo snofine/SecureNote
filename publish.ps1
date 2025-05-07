4e8f53447afc888defca5938798176adcf36e65a
Sala1at <sala1at@users.noreply.github.com>
2025-05-07 18:41:24 +0300

    Удален файл publish.ps1

--- publish.ps1
+++
@@ -1,8 +0,0 @@
-# Очистка предыдущей сборки
-Remove-Item -Path "bin\Release\net7.0-windows\publish" -Recurse -Force -ErrorAction SilentlyContinue
-
-# Публикация приложения
-dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
-
-# Создание установщика
-& "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "setup.iss" 
